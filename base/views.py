1b18c83dfc9a81392c937b0ebf9ed34d7d1c0175
nithin <sureshnithin1729@gmail.com>
2024-06-11 23:29:51 +0530

    actvity page done

--- base/views.py
+++ base/views.py
@@ -61,13 +61,22 @@ def registerPage(request):
     return render(request,'base/login_register.html',context)
 
 def home(request):
-    q=request.GET.get('q') if request.GET.get('q')!=None else ''
-    rooms=Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)) #query
-    #return HttpResponse("Home Page")
-    topics=Topic.objects.all()
-    room_count=rooms.count()
-    context={'rooms':rooms,'topics':topics,'room_count':room_count}
-    return render(request,'base/home.html',context)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()[0:5]
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))[0:3]
+
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
+    return render(request, 'base/home.html', context)
 
 def room(request,pk):
     #return HttpResponse("Room")
