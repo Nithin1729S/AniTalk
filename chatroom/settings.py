756372e6085f31a3fd3b442cea6a32409d44caaf
nithin <sureshnithin1729@gmail.com>
2024-06-12 17:08:18 +0530

    vercel json added

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0sect#^v=bcj^*g-cot@mgw(^#ghp)$t(r61_%f6tbh2x41x2u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
